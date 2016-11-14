fd7eb57d505f0362efe47c371437eec7cc4c4ec8
Benoit Belz <bbelz@lucca.fr>
2016-11-14 17:39:06 +0100

    [ADD] FF-710 : IPublisher

---
+++ src/MerQure/IPublisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerQure
+{
+    interface IPublisher
+    {
+        string ExchangeName { get; }
+
+        void Declare();
+
+        void Publish(IMessage message);
+    }
+}
