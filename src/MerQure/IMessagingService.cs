62af0b7d9fdf61a0765f2badacf5c2030fa4d28e
Benoit Belz <bbelz@lucca.fr>
2016-11-14 17:40:48 +0100

    [ADD] FF-708 : IMessagingService

---
+++ src/MerQure/IMessagingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerQure
+{
+    // TODO rename ?
+    interface IMessagingService
+    {
+        IPublisher GetPublisher(string exchangeName);
+
+        ISubscriber GetSubscriber(string queueName);
+
+        IConsumer GetConsumer(string queueName, bool noAck);
+    }
+}
