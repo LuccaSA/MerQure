4642af0e75792f64ecbb19e918b0c92b8a0de69e
Benoit Belz <bbelz@lucca.fr>
2016-11-14 17:36:55 +0100

    [ADD] FF-711 : IConsumer

---
+++ src/MerQure/IConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerQure
+{
+    interface IConsumer
+    {
+        string QueueName { get; }
+
+        bool WithAcknowledgement { get; }
+
+        void Consume(EventHandler<IMessagingEvent> OnMessageReceived);
+
+        void AcknowlegdeDeliveredMessage(IMessagingEvent args);
+
+        void RejectDeliveredMessage(IMessagingEvent args);
+    }
+}
