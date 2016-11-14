e2e94764abfc60c7d3215cab9eab897190bd960e
Benoit Belz <bbelz@lucca.fr>
2016-11-14 17:37:30 +0100

    [ADD] FF-709 : ISubscriber

---
+++ src/MerQure/ISubscriber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerQure
+{
+    interface ISubscriber
+    {
+        string QueueName { get; }
+
+        void DeclareSubscribtion(string exchangeName, string routingKey);
+    }
+}
