9aa7c78121d5e7ca1100069247362c51c3d4b544
Benoit Belz <bbelz@lucca.fr>
2016-11-15 17:06:31 +0100

    [ADD] RabbitMQ useful classes

---
+++ src/MerQure.RabbitMQ/Content/Header.cs
@@ -0,0 +1,20 @@
+﻿using MerQure.Content;
+using System.Collections.Generic;
+
+namespace MerQure.RabbitMQ.Content
+{
+    class Header : IHeader
+    {
+        private IDictionary<string, object> HeaderProperties;
+
+        public Header(IDictionary<string, object> headerProperties)
+        {
+            this.HeaderProperties = headerProperties;
+        }
+
+        public IDictionary<string, object> GetHeaderProperties()
+        {
+            return HeaderProperties;
+        }
+    }
+}
---
+++ src/MerQure.RabbitMQ/Events/MessagingEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace MerQure.RabbitMQ.Events
+{
+    class MessagingEvent : IMessagingEvent
+    {
+        public IMessage Message { get; set; }
+
+        public string DeliveryTag { get; set; }
+
+        public MessagingEvent(IMessage Message, string DeliveryTag)
+        {
+            this.Message = Message;
+            this.DeliveryTag = DeliveryTag;
+        }
+    }
+}
