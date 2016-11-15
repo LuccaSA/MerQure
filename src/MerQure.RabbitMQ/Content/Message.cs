c404f91244f7a6e1eb75d3081c52e6104c44c3f2
Benoit Belz <bbelz@lucca.fr>
2016-11-15 17:06:12 +0100

    [ADD] FF-717 : RabbitMQ Message

---
+++ src/MerQure.RabbitMQ/Content/Message.cs
@@ -0,0 +1,24 @@
+﻿using MerQure.Content;
+
+namespace MerQure.RabbitMQ
+{
+    public class Message : IMessage
+    {
+        public string RoutingKey { get; }
+        public IHeader Header { get; }
+        public string Body { get; set; }
+
+        /// <summary>
+        /// Create a RabbitMQ Message
+        /// </summary>
+        /// <param name="routingKey"></param>
+        /// <param name="header"></param>
+        /// <param name="body"></param>
+        public Message(string routingKey, IHeader header, string body)
+        {
+            this.RoutingKey = RoutingKey;
+            this.Header = Header;
+            this.Body = Body;
+        }
+    }
+}
