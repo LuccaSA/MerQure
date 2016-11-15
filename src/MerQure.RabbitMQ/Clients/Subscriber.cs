99e2994c37c90ca982e9d8cd4013728d9651a513
Benoit Belz <bbelz@lucca.fr>
2016-11-15 17:05:20 +0100

    [ADD] FF-721 : RabbitMQ Subscriber

---
+++ src/MerQure.RabbitMQ/Clients/Subscriber.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System.Collections.Generic;
+
+namespace MerQure.RabbitMQ.Clients
+{
+    class Subscriber : RabbitMQClient, ISubscriber
+    {
+        private bool queueDeclared = false;
+
+        public string QueueName { get; private set; }
+        public bool Durable { get; private set; }
+        public bool AutoDeleteQueue { get; private set; }
+
+        /// <summary>
+        /// Declare a  RabbitMQ Subscriber
+        /// </summary>
+        /// <param name="channel"></param>
+        /// <param name="queueName"></param>
+        /// <param name="durable"></param>
+        /// <param name="autoDeleteQueue"></param>
+        public Subscriber(IModel channel, string queueName, bool durable, bool autoDeleteQueue)
+            : base(channel)
+        {
+            this.QueueName = queueName.ToLowerInvariant();
+            this.Durable = durable;
+            this.AutoDeleteQueue = autoDeleteQueue;
+        }
+
+        public void DeclareSubscribtion(string exchangeName, string routingKey)
+        {
+            if (!queueDeclared)
+            {
+                var queueArgs = new Dictionary<string, object>();
+
+                this.Channel.QueueDeclare(this.QueueName, this.Durable, false, this.AutoDeleteQueue, queueArgs);
+                queueDeclared = true;
+            }
+
+            this.Channel.QueueBind(this.QueueName, exchangeName.ToLowerInvariant(), routingKey.ToLowerInvariant());
+        }
+    }
+}
