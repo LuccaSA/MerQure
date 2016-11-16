fb3b838a962aae4185779f94a319f52b8ea1256f
Benoit Belz <bbelz@lucca.fr>
2016-11-16 11:05:15 +0100

    [ADD] SimpleExample Sample

---
+++ src/MerQure.Samples/SimpleExample.cs
@@ -0,0 +1,29 @@
+﻿using MerQure.RMQ;
+using MerQure.RMQ.Content;
+using System;
+using System.Collections.Generic;
+
+namespace MerQure.Samples
+{
+    class SimpleExample
+    {
+        static void Main(string[] args)
+        {
+            var messagingService = new MessagingService();
+
+            messagingService.GetPublisher("simple.exchange").Declare();
+
+            messagingService.GetSubscriber("simple.queue").DeclareSubscribtion("simple.exchange", "simple.key");
+
+            var headerProp = new Dictionary<string, object> { { "header-property", "property-value" } };
+            var header = new Header(headerProp);
+            var message = new Message("simple.key", header, "TEST_BODY");
+            messagingService.GetPublisher("simple.exchange").Publish(message);
+
+            messagingService.GetConsumer("simple.queue").Consume((object sender, IMessagingEvent e) => 
+            {
+                Console.WriteLine(e.Message.Body);
+            });
+        }
+    }
+}
