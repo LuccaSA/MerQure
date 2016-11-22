5597b901238e77dc4b6dd5ec8be5458df7816191
Benoit Belz <bbelz@lucca.fr>
2016-11-22 15:29:41 +0100

    [REF] Tab to spaces

--- src/MerQure/Clients/IConsumer.cs
+++ src/MerQure/Clients/IConsumer.cs
@@ -2,32 +2,32 @@
 
 namespace MerQure
 {
-	/// <summary>
-	/// Consumer interface. Used to receive messages from a queue by subscription.
-	/// </summary>
-	public interface IConsumer
+    /// <summary>
+    /// Consumer interface. Used to receive messages from a queue by subscription.
+    /// </summary>
+    public interface IConsumer
     {
-		/// <summary>
-		/// name of the queue subscribed by the consumer
-		/// </summary>
-		string QueueName { get; }
+        /// <summary>
+        /// name of the queue subscribed by the consumer
+        /// </summary>
+        string QueueName { get; }
 
-		/// <summary>
-		/// Start listening on the queue
-		/// </summary>
-		/// <param name="OnMessageReceived">Handler called each time a message arrives for this consumer.</param>
-		void Consume(EventHandler<IMessagingEvent> OnMessageReceived);
+        /// <summary>
+        /// Start listening on the queue
+        /// </summary>
+        /// <param name="OnMessageReceived">Handler called each time a message arrives for this consumer.</param>
+        void Consume(EventHandler<IMessagingEvent> OnMessageReceived);
 
-		/// <summary>
-		/// Acknowledge a delivered message.
-		/// </summary>
-		/// <param name="args"></param>
-		void AcknowlegdeDeliveredMessage(IMessagingEvent args);
+        /// <summary>
+        /// Acknowledge a delivered message.
+        /// </summary>
+        /// <param name="args"></param>
+        void AcknowlegdeDeliveredMessage(IMessagingEvent args);
 
-		/// <summary>
-		/// Reject a delivered message.
-		/// </summary>
-		/// <param name="args"></param>
-		void RejectDeliveredMessage(IMessagingEvent args);
+        /// <summary>
+        /// Reject a delivered message.
+        /// </summary>
+        /// <param name="args"></param>
+        void RejectDeliveredMessage(IMessagingEvent args);
     }
 }
--- src/MerQure/Clients/IPublisher.cs
+++ src/MerQure/Clients/IPublisher.cs
@@ -1,24 +1,24 @@
 ﻿namespace MerQure
 {
-	/// <summary>
-	/// Publisher interface. Used to publish messages on an exchange.
-	/// </summary>
-	public interface IPublisher
+    /// <summary>
+    /// Publisher interface. Used to publish messages on an exchange.
+    /// </summary>
+    public interface IPublisher
     {
-		/// <summary>
-		/// Exhange where publish messages
-		/// </summary>
+        /// <summary>
+        /// Exhange where publish messages
+        /// </summary>
         string ExchangeName { get; }
 
-		/// <summary>
-		/// Declare an exchange
-		/// </summary>
-		void Declare();
+        /// <summary>
+        /// Declare an exchange
+        /// </summary>
+        void Declare();
 
-		/// <summary>
-		/// Publishes a message
-		/// </summary>
-		/// <param name="message"></param>
-		void Publish(IMessage message);
+        /// <summary>
+        /// Publishes a message
+        /// </summary>
+        /// <param name="message"></param>
+        void Publish(IMessage message);
     }
 }
