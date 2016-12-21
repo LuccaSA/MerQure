6f9881ec64114eaf41a1343218d8adbc6121b0d1
Brice <bfrancois@lucca.fr>
2016-12-21 17:49:20 +0100

    [ADD] Test Project

---
+++ src/MerQure.RbMQ.Tests/MessagingServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+
+namespace MerQure.RbMQ.Tests
+{
+    public class MessagingServiceTests
+    {
+        [Fact]
+        public void DeclareExchangeFailWithEmptyName()
+        {
+            var messagingService = new MessagingService();
+
+            Assert.Throws<ArgumentNullException>(() => messagingService.DeclareExchange(null));
+            Assert.Throws<ArgumentNullException>(() => messagingService.DeclareExchange("    "));
+        }
+
+        [Fact]
+        public void DeclareQueueFailWithEmptyName()
+        {
+            var messagingService = new MessagingService();
+
+            Assert.Throws<ArgumentNullException>(() => messagingService.DeclareQueue(null));
+            Assert.Throws<ArgumentNullException>(() => messagingService.DeclareQueue("    "));
+        }
+
+        [Fact]
+        public void DeclareBindingFailWithEmptyParameters()
+        {
+            var messagingService = new MessagingService();
+
+            Assert.Throws<ArgumentNullException>(() => messagingService.DeclareBinding(null, "queue", "routing"));
+            Assert.Throws<ArgumentNullException>(() => messagingService.DeclareBinding("exhange", null, "routing"));
+            Assert.Throws<ArgumentNullException>(() => messagingService.DeclareBinding("exhange", "queue", null));
+        }
+
+        [Fact]
+        public void CancelBindingFailWithEmptyParameters()
+        {
+            var messagingService = new MessagingService();
+
+            Assert.Throws<ArgumentNullException>(() => messagingService.CancelBinding(null, "queue", "routing"));
+            Assert.Throws<ArgumentNullException>(() => messagingService.CancelBinding("exhange", null, "routing"));
+            Assert.Throws<ArgumentNullException>(() => messagingService.CancelBinding("exhange", "queue", null));
+        }
+    }
+}
